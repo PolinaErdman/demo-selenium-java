093777964a90066bb050ce7f740efd9210b79e95
palinaerdman <polinaerdman@gmail.com>
2024-10-27 18:15:20 +0300

    refactor message and xpath

--- src/test/java/com/netflix/LoginMessage.java
+++ src/test/java/com/netflix/LoginMessage.java
@@ -1,7 +1,8 @@
 package com.netflix;
 
 public class LoginMessage {
-    public static final String INVALID_EMAIL_OR_PHONE = "Please enter a valid email or phone number.";
-    public static final String INVALID_PASSWORD_PHONE = "Your password must contain between 4 and 60 characters.";
-    public static final String INVALID_EMAIL_OR_PHONE_AND_PASSWORD = "Incorrect password for";
+    public static final String INVALID_EMAIL_OR_PHONE = "Укажите действительный адрес электронной почты или номер телефона.";
+    public static final String INVALID_PASSWORD = "Пароль должен содержать от 4 до 60 символов.";
+    public static final String INVALID_EMAIL_OR_PHONE_AND_PASSWORD = "Неверный пароль для dfgdfg\n" +
+            "Вы можете использовать код для входа, сбросить пароль или повторить попытку.";
 }
--- src/test/java/com/netflix/LoginXpath.java
+++ src/test/java/com/netflix/LoginXpath.java
@@ -1,7 +1,10 @@
 package com.netflix;
 
 public class LoginXpath {
-    public static final String INPUT_EMAIL_OR_NUMBER_XPATH = "/html/body/div[1]/div/div/div[2]/div/form/div[1]/div/div[1]/input";
-    public static final String INPUT_PASSWORD_XPATH = "/html/body/div[1]/div/div/div[2]/div/form/div[2]/div/div[1]/input";
+    public static final String INPUT_EMAIL_OR_PHONE_XPATH = "/html/body/div[1]/div/div/div[2]/div/form/div[1]/div/div[1]/input";
+    public static final String INPUT_PASSWORD_XPATH = "/html/body/div[1]/div/div/div[2]/div/form/div[2]/div/div/input";
     public static final String BUTTON_SIGN_IN_XPATH = "/html/body/div[1]/div/div/div[2]/div/form/button[1]";
+    public static final String INVALID_EMAIL_OR_PHONE_MESSAGE_XPATH = "/html/body/div[1]/div/div/div[2]/div/form/div[1]/div/div[2]";
+    public static final String INVALID_PASSWORD_MESSAGE_XPATH = "/html/body/div[1]/div/div/div[2]/div/form/div[2]/div/div[2]";
+    public static final String INVALID_CREDENTIALS_MESSAGE_XPATH = "/html/body/div[1]/div/div/div[2]/div/header/div/div/div";
 }
