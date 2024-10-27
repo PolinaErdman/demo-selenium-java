f828f0508c2d6d0b8e38bc72c10dc72748ac2407
palinaerdman <polinaerdman@gmail.com>
2024-10-27 20:40:44 +0300

    refactor LoginTest (add test emptyPhone, emptyPassword)

--- src/test/java/by/emall/LoginTest.java
+++ src/test/java/by/emall/LoginTest.java
@@ -10,7 +10,7 @@ import java.util.concurrent.TimeUnit;
 public class LoginTest {
 
     @Test
-    public void test1() throws InterruptedException {
+    public void testEmptyPhoneAndPassword() throws InterruptedException {
         WebDriver driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.get("https://emall.by/login/password");
@@ -19,6 +19,34 @@ public class LoginTest {
         loginPage.clickButtonSignIn();
         String actual = loginPage.getErrorMessageText();
         String expected = LoginMessage.EMPTY_PHONE_AND_PASSWORD;
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testEmptyPassword() throws InterruptedException {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get("https://emall.by/login/password");
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.clickButtonCookieAccept();
+        loginPage.sendKeysInputPhone("331000000");
+        loginPage.clickButtonSignIn();
+        String actual = loginPage.getErrorMessageText();
+        String expected = LoginMessage.EMPTY_PASSWORD;
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testEmptyPhone() throws InterruptedException {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get("https://emall.by/login/password");
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.clickButtonCookieAccept();
+        loginPage.sendKeysInputPassword("testtest");
+        loginPage.clickButtonSignIn();
+        String actual = loginPage.getErrorMessageText();
+        String expected = LoginMessage.EMPTY_PHONE;
         Assertions.assertEquals(expected, actual);
     }
 }
