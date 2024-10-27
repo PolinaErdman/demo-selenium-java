7e647ed60bb35c7fb74e986f69a1c0bd50e5a3cf
palinaerdman <polinaerdman@gmail.com>
2024-10-27 17:32:27 +0300

    refactor XPATH

--- src/test/java/com/netflix/LoginXpath.java
+++ src/test/java/com/netflix/LoginXpath.java
@@ -1,7 +1,7 @@
 package com.netflix;
 
 public class LoginXpath {
-    public static final String INPUT_EMAIL_NUMBER_XPATH = "/html/body/div[1]/div/div/div[2]/div/form/div[1]/div/div[1]/input";
+    public static final String INPUT_EMAIL_OR_NUMBER_XPATH = "/html/body/div[1]/div/div/div[2]/div/form/div[1]/div/div[1]/input";
     public static final String INPUT_PASSWORD_XPATH = "/html/body/div[1]/div/div/div[2]/div/form/div[2]/div/div[1]/input";
     public static final String BUTTON_SIGN_IN_XPATH = "/html/body/div[1]/div/div/div[2]/div/form/button[1]";
 }
