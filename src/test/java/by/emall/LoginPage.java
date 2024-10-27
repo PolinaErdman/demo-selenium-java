51ac06989957739d9d4e9106703cf3dd43b26afa
palinaerdman <polinaerdman@gmail.com>
2024-10-27 20:01:53 +0300

    add LoginPage

---
+++ src/test/java/by/emall/LoginPage.java
@@ -0,0 +1,37 @@
+package by.emall;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class LoginPage {
+    private WebDriver driver;
+
+    public LoginPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void clickButtonSignIn() {
+        By buttonSignInBy = By.xpath(LoginXpath.BUTTON_SIGN_IN_XPATH);
+        WebElement buttonSignInWebElement = driver.findElement(buttonSignInBy);
+        buttonSignInWebElement.click();
+    }
+
+    public String getErrorMessageText() {
+        By textErrorMessageBy = By.xpath(LoginXpath.ERROR_MESSAGE_XPATH);
+        WebElement textErrorMessageWebElement = driver.findElement(textErrorMessageBy);
+        return textErrorMessageWebElement.getText();
+    }
+
+    public void sendKeysInputPhone(String phone) {
+        By inputPhoneBy = By.xpath(LoginXpath.INPUT_PHONE_XPATH);
+        WebElement inputPhoneWebElement = driver.findElement(inputPhoneBy);
+        inputPhoneWebElement.sendKeys(phone);
+    }
+
+    public void sendKeysInputPassword(String password) {
+        By inputPasswordBy = By.xpath(LoginXpath.INPUT_PASSWORD_XPATH);
+        WebElement inputPasswordWebElement = driver.findElement(inputPasswordBy);
+        inputPasswordWebElement.sendKeys(password);
+    }
+}
