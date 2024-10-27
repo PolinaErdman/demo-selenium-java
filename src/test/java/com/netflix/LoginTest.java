9fa643c0771a7180dda25db871da82e4a5252d34
palinaerdman <polinaerdman@gmail.com>
2024-10-27 18:32:44 +0300

    add test 2 empty password, test 3 empty email

--- src/test/java/com/netflix/LoginTest.java
+++ src/test/java/com/netflix/LoginTest.java
@@ -20,6 +20,22 @@ public class LoginTest {
 
     @Test
     public void test2() {
-
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.netflix.com/login");
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.sendKeysInputEmailOrPhone("test@test.com");
+        loginPage.clickButtonSignIn();
+        Assertions.assertEquals(LoginMessage.INVALID_PASSWORD, loginPage.getPasswordErrorMessageText());
     }
+
+    @Test
+    public void test3() {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.netflix.com/login");
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.sendKeysPassword("testpassword");
+        loginPage.clickButtonSignIn();
+        Assertions.assertEquals(LoginMessage.INVALID_EMAIL_OR_PHONE, loginPage.getEmailOrPhoneErrorMessageText());
+    }
+
 }
