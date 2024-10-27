013020e45930b7a637bf846f69d7c323a48740fe
palinaerdman <polinaerdman@gmail.com>
2024-10-27 17:31:57 +0300

    add LoginMessage

---
+++ src/test/java/com/netflix/LoginMessage.java
@@ -0,0 +1,7 @@
+package com.netflix;
+
+public class LoginMessage {
+    public static final String INVALID_EMAIL_OR_PHONE = "Please enter a valid email or phone number.";
+    public static final String INVALID_PASSWORD_PHONE = "Your password must contain between 4 and 60 characters.";
+    public static final String INVALID_EMAIL_OR_PHONE_AND_PASSWORD = "Incorrect password for";
+}
