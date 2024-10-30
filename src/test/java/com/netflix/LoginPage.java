0b57d0cda0bcb262807f934ad64addba4dae253c
palinaerdman <polinaerdman@gmail.com>
2024-10-31 00:09:15 +0300

    refactor LoginPage

--- src/test/java/com/netflix/LoginPage.java
+++ src/test/java/com/netflix/LoginPage.java
@@ -12,37 +12,37 @@ public class LoginPage {
     }
 
     public void clickButtonSignIn() {
-        By buttonSignInBy = By.xpath(LoginXpath.BUTTON_SIGN_IN_XPATH);
+        By buttonSignInBy = By.xpath(LoginXpath.BUTTON_SIGN_IN_LOCATOR);
         WebElement buttonSignInWebElement = driver.findElement(buttonSignInBy);
         buttonSignInWebElement.click();
     }
 
     public String getEmailOrPhoneErrorMessageText() {
-        By textEmailOrPhoneErrorMessageBy = By.xpath(LoginXpath.INVALID_EMAIL_OR_PHONE_MESSAGE_XPATH);
+        By textEmailOrPhoneErrorMessageBy = By.xpath(LoginXpath.INVALID_EMAIL_OR_PHONE_MESSAGE_LOCATOR);
         WebElement textEmailOrPhoneErrorMessageWebElement = driver.findElement(textEmailOrPhoneErrorMessageBy);
         return textEmailOrPhoneErrorMessageWebElement.getText();
     }
 
     public String getPasswordErrorMessageText() {
-        By textPasswordErrorMessageBy = By.xpath(LoginXpath.INVALID_PASSWORD_MESSAGE_XPATH);
+        By textPasswordErrorMessageBy = By.xpath(LoginXpath.INVALID_PASSWORD_MESSAGE_LOCATOR);
         WebElement textPasswordErrorMessageWebElement = driver.findElement(textPasswordErrorMessageBy);
         return textPasswordErrorMessageWebElement.getText();
     }
 
     public void sendKeysInputEmailOrPhone(String emailOrPhone) {
-        By inputEmailOrPhoneBy = By.xpath(LoginXpath.INPUT_EMAIL_OR_PHONE_XPATH);
+        By inputEmailOrPhoneBy = By.xpath(LoginXpath.INPUT_EMAIL_OR_PHONE_LOCATOR);
         WebElement inputEmailOrPhoneWebElement = driver.findElement(inputEmailOrPhoneBy);
         inputEmailOrPhoneWebElement.sendKeys(emailOrPhone);
     }
 
     public void sendKeysInputPassword(String password) {
-        By inputPasswordBy = By.xpath(LoginXpath.INPUT_PASSWORD_XPATH);
+        By inputPasswordBy = By.xpath(LoginXpath.INPUT_PASSWORD_LOCATOR);
         WebElement inputPasswordWebElement = driver.findElement(inputPasswordBy);
         inputPasswordWebElement.sendKeys(password);
     }
 
     public String getCredentialsErrorMessageText() {
-        By textCredentialsErrorMessageBy = By.xpath(LoginXpath.INVALID_CREDENTIALS_MESSAGE_XPATH);
+        By textCredentialsErrorMessageBy = By.xpath(LoginXpath.INVALID_CREDENTIALS_MESSAGE_LOCATOR);
         WebElement textCredentialsErrorMessageWebElement = driver.findElement(textCredentialsErrorMessageBy);
         return textCredentialsErrorMessageWebElement.getText();
     }
