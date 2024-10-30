d9db747176d0892019c35aff60cb43621122575d
palinaerdman <polinaerdman@gmail.com>
2024-10-31 00:08:18 +0300

    refactor LoginXpath (changed locators)

--- src/test/java/com/netflix/LoginXpath.java
+++ src/test/java/com/netflix/LoginXpath.java
@@ -1,10 +1,10 @@
 package com.netflix;
 
 public class LoginXpath {
-    public static final String INPUT_EMAIL_OR_PHONE_XPATH = "/html/body/div[1]/div/div/div[2]/div/form/div[1]/div/div[1]/input";
-    public static final String INPUT_PASSWORD_XPATH = "/html/body/div[1]/div/div/div[2]/div/form/div[2]/div/div/input";
-    public static final String BUTTON_SIGN_IN_XPATH = "/html/body/div[1]/div/div/div[2]/div/form/button[1]";
-    public static final String INVALID_EMAIL_OR_PHONE_MESSAGE_XPATH = "/html/body/div[1]/div/div/div[2]/div/form/div[1]/div/div[2]";
-    public static final String INVALID_PASSWORD_MESSAGE_XPATH = "/html/body/div[1]/div/div/div[2]/div/form/div[2]/div/div[2]";
-    public static final String INVALID_CREDENTIALS_MESSAGE_XPATH = "/html/body/div[1]/div/div/div[2]/div/header/div/div/div/div";
+    public static final String INPUT_EMAIL_OR_PHONE_LOCATOR = "//input[@name='userLoginId']";
+    public static final String INPUT_PASSWORD_LOCATOR = "//input[@name='password']";
+    public static final String BUTTON_SIGN_IN_LOCATOR = "//button[@type='submit']";
+    public static final String INVALID_EMAIL_OR_PHONE_MESSAGE_LOCATOR = "//div[@data-uia='login-field+validationMessage']";
+    public static final String INVALID_PASSWORD_MESSAGE_LOCATOR = "//div[@data-uia='password-field+validationMessage']";
+    public static final String INVALID_CREDENTIALS_MESSAGE_LOCATOR = "//div[@data-uia='error-message-container+header']";
 }
